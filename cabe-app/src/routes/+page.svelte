d0c28778ff7eab92a69343782f78f292ab1ecb74
Peter Kalverla <peter.kalverla@gmx.com>
2023-05-24 14:47:43 +0200

    tabs as component

--- cabe-app/src/routes/+page.svelte
+++ cabe-app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { each } from "svelte/internal";
+    import Tabs from '$lib/Tabs.svelte';
 
 	const strategies = [
         {code: 'GF', description: 'Grandfathering'},
@@ -16,58 +17,4 @@
 
 <h1>FAIR Carbon Budget Explorer</h1>
 <p>Pick your effort-sharing principle</p>
-
-<ul>
-    {#each strategies as strategy}
-        <li class={activeStrategy === strategy.code ? 'active' : ''}>
-            <span on:click={handleClick(strategy.code)}>{strategy.description}</span>
-        </li>
-    {/each}
-</ul>
-{#each strategies as strategy}
-	{#if activeStrategy == strategy.code}
-	<div class="box">
-		<p>{strategy.description}</p>
-	</div>
-	{/if}
-{/each}
-
-<style>
-	.box {
-		margin-bottom: 10px;
-		padding: 40px;
-		border: 1px solid #dee2e6;
-    border-radius: 0 0 .5rem .5rem;
-    border-top: 0;
-	}
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    padding-left: 0;
-    margin-bottom: 0;
-    list-style: none;
-    border-bottom: 1px solid #dee2e6;
-  }
-	li {
-		margin-bottom: -1px;
-	}
-
-  span {
-    border: 1px solid transparent;
-    border-top-left-radius: 0.25rem;
-    border-top-right-radius: 0.25rem;
-    display: block;
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-  }
-
-  span:hover {
-    border-color: #e9ecef #e9ecef #dee2e6;
-  }
-
-  li.active > span {
-    color: #495057;
-    background-color: #fff;
-    border-color: #dee2e6 #dee2e6 #fff;
-  }
-</style>
+<Tabs {strategies} />
