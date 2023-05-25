2093c9e343f4352ada41f366387873ef9ad3cc3d
Peter Kalverla <peter.kalverla@gmx.com>
2023-05-25 08:32:20 +0200

    Add hover effect

--- cabe-app/src/lib/Map.svelte
+++ cabe-app/src/lib/Map.svelte
@@ -1,32 +1,49 @@
 <!-- https://dev.to/learners/maps-with-d3-and-svelte-8p3 -->
 <script>
-    import { geoPath, geoNaturalEarth1 } from  "d3";
+    import { onMount } from 'svelte';
+    import { json, select, geoPath, geoNaturalEarth1 } from  "d3";
+    const d3 = { json, select, geoPath, geoNaturalEarth1 }
+
+    let dataset = [];
+    onMount(async () => {
+        // const countriesURL = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+        const countriesURL = "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson"
+        // const countriesURL = "/ne_110m_admin_0_countries.geojson"
+        const countries = await json(countriesURL);
+        dataset = countries.features;
+    });
 
     const width = 1200;
     const height = 600;
 
-    const projection = geoNaturalEarth1();
-    const path = geoPath(projection);
+    const projection = d3.geoNaturalEarth1();
+    const path = d3.geoPath(projection);
 
-    import { json } from  "d3";
-    // const countriesURL = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-    const countriesURL = "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson"
-    let dataset = [];
-    json(countriesURL).then((data) => {
-        dataset = data.features;
-    })
+    function handleMouseover(event) {
+        d3.select(this).attr("fill", 'red')
+        d3.select(this).attr("stroke-width", '1.5')
+    }
+    function handleMouseout(event) {
+        d3.select(this).attr("fill", 'black')
+        d3.select(this).attr("stroke-width", '0.5')
+
+    }
 
 </script>
 
 <svg {width} {height}>
 {#each dataset as data}
-    <path d={path(data)} />
+    <path
+     d={path(data)}
+     stroke="white"
+     stroke-width="0.5"
+     fill="black"
+     on:mouseover={handleMouseover}
+     on:focus={handleMouseover}
+     on:mouseout={handleMouseout}
+     on:blur={handleMouseout}
+     />
+     <!-- TODO: can we set attributes reactively? -->
+     <!-- fill={geoContains(this, projection.invert(mouse)) ? "red" : "black"} -->
 {/each}
-
 </svg>
-
-<style>
-    path {
-        stroke:white;
-    }
-</style>
