48483c2c9d22eb0a18025f83367cf33316a85fe9
Peter Kalverla <peter.kalverla@gmx.com>
2023-06-07 17:00:03 +0200

    Vega map now works with geojson, joined data from svelte, and can interact with svelte outside vega graph

--- src/lib/VegaMap.svelte
+++ src/lib/VegaMap.svelte
@@ -1,51 +1,86 @@
 <script lang="ts">
-	import type { VisualizationSpec } from 'svelte-vega';
+	import type { SignalListeners, VisualizationSpec } from 'svelte-vega';
 	import { Vega } from 'svelte-vega';
+	import type { Metric } from './metrics';
+
+	export let metrics: Metric[];
+	export let country: any;
+
+	function updateCountry(_: string, value: unknown) {
+		country = value;
+	}
+
+	const signalListeners: SignalListeners = {
+		selectedCountry: updateCountry
+	};
 
 	const spec: VisualizationSpec = {
 		$schema: 'https://vega.github.io/schema/vega/v5.json',
-		description: 'A configurable map of countries of the world.',
-		width: 900,
+		description: 'Test map',
+		width: 960,
 		height: 500,
 		autosize: 'none',
+		signals: [
+			{
+				name: 'selectedCountry',
+				description: 'A country ISO code that updates in response to mouse click',
+				value: 'NLD',
+				on: [{ events: 'mousedown', update: "datum ? datum.properties.ISO_A3_EH : ''" }] // ISO_A3 not set for FRA
+			}
+		],
 		data: [
 			{
-				name: 'world',
-				url: 'https://raw.githubusercontent.com/vega/vega-datasets/main/data/world-110m.json',
-				format: {
-					type: 'topojson',
-					feature: 'countries'
-				}
+				name: 'metrics',
+				values: metrics,
+				format: { type: 'json', parse: 'auto' }
+				// async: true,
 			},
+			{
+				name: 'countries',
+				// url: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson',
+				url: '/ne_110m_admin_0_countries.geojson',
+				format: { type: 'json', property: 'features' },
+				transform: [
+					{
+						type: 'lookup',
+						from: 'metrics',
+						key: 'ISO',
+						fields: ['properties.ISO_A3_EH'], // fields from countries for joining the data
+						values: ['Population', 'GDP'], // fields from metrics to add to the data
+						default: 'unknown'
+					}
+				]
+			}
 		],
-        projections: [
-            {
-            name: 'naturalEarth1',
-            type: 'naturalEarth1'
-            },
-        ],
+		projections: [{ name: 'projection', type: 'naturalEarth1' }],
+		scales: [
+			{
+				name: 'color',
+				type: 'log',
+				domain: { data: 'countries', field: 'GDP' },
+				range: { scheme: 'cividis' }
+			}
+		],
+		legends: [
+			{
+				fill: 'color',
+				orient: 'bottom-left',
+				title: 'Median GDP'
+			}
+		],
 		marks: [
 			{
 				type: 'shape',
-				from: { data: 'world' },
+				from: { data: 'countries' },
 				encode: {
-					update: {
-						strokeWidth: { value: 2 },
-						stroke: { value: '#bbb' },
-						fill: { value: '#000' },
-						zindex: { value: 0 }
-					},
-					hover: {
-						strokeWidth: { value: 2 },
-						stroke: { value: 'firebrick' },
-						zindex: { value: 1 }
-					}
+					enter: { tooltip: { field: 'properties.GDP_MD' } }, // Note this is population from NE world data
+					update: { fill: { scale: 'color', field: 'GDP' } }, // Note this is population from other source
+					hover: { fill: { value: 'red' } }
 				},
-				transform: [{ type: 'geoshape', projection: 'naturalEarth1' }]
+				transform: [{ type: 'geoshape', projection: 'projection' }]
 			}
 		]
 	};
 </script>
 
-<Vega {spec} />
-
+<Vega {spec} {signalListeners} />
--- src/routes/world/+page.svelte
+++ src/routes/world/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	// import VegaLiteMap from "$lib/VegaLiteMap.svelte";
 	import VegaMap from "$lib/VegaMap.svelte";
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	let country: string;
 </script>
+
 <h1>World explorer</h1>
-<p>Make a selection and view the map!</p>
+<p>Make a selection by clicking on a country.</p>
+<p>Selected country: {country}</p>
 <a href="/">Go back</a>
-<!-- <Map metrics={data.metrics} /> -->
-<VegaMap />
+<VegaMap metrics={data.metrics} bind:country />
