47acbc5cd421e2d176059dbb2b12e1fe42a0d75d
Peter Kalverla <peter.kalverla@gmx.com>
2023-06-12 10:58:26 +0200

    wip - add year filter

--- src/routes/world/+page.server.ts
+++ src/routes/world/+page.server.ts
@@ -1,7 +1,22 @@
-import { loadMetrics } from "$lib/metrics";
+import { loadMetrics, type Metric } from "$lib/metrics";
 
-export async function load() {
-    return {
-        metrics: await loadMetrics()
+function filterMetrics(raw_metrics: Metric[], year: number) {
+    return raw_metrics.filter(d => d.Time === year)
+}
+
+export async function load({ url }) {
+
+    // TODO validate with zod.js
+
+    let rawyear: any = url.searchParams.get('year')
+    let year: number = 2018
+    if (rawyear !== undefined) {
+        console.log(year)
+        year = parseInt(rawyear)
     }
+
+    console.log(year)
+    const raw_metrics = await loadMetrics()
+    const metrics = filterMetrics(raw_metrics, year)
+    return { metrics }
 }
