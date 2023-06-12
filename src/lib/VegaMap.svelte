3b563d80c8f19115b2812b19a8f37560017341e7
sverhoeven <s.verhoeven@esciencecenter.nl>
2023-06-12 16:01:10 +0200

    Make world map zoomable and pannable
    Fixes #8

--- src/lib/VegaMap.svelte
+++ src/lib/VegaMap.svelte
@@ -27,6 +27,89 @@
 				description: 'A country ISO code that updates in response to mouse click',
 				value: 'NLD',
 				on: [{ events: 'mousedown', update: "datum ? datum.properties.ISO_A3_EH : ''" }] // ISO_A3 not set for FRA
+			},
+			{ name: 'tx', update: 'width / 2' },
+			{ name: 'ty', update: 'height / 2' },
+			{
+				name: 'scale',
+				value: 150,
+				on: [
+					{
+						events: { type: 'wheel', consume: true },
+						update:
+							'clamp(scale * pow(1.0005, -event.deltaY * pow(16, event.deltaMode)), 150, 3000)'
+					}
+				]
+			},
+			{
+				name: 'angles',
+				value: [0, 0],
+				on: [
+					{
+						events: 'mousedown',
+						update: '[rotateX, centerY]'
+					}
+				]
+			},
+			{
+				name: 'cloned',
+				value: null,
+				on: [
+					{
+						events: 'mousedown',
+						update: "copy('projection')"
+					}
+				]
+			},
+			{
+				name: 'start',
+				value: null,
+				on: [
+					{
+						events: 'mousedown',
+						update: 'invert(cloned, xy())'
+					}
+				]
+			},
+			{
+				name: 'drag',
+				value: null,
+				on: [
+					{
+						events: '[mousedown, window:mouseup] > window:mousemove',
+						update: 'invert(cloned, xy())'
+					}
+				]
+			},
+			{
+				name: 'delta',
+				value: null,
+				on: [
+					{
+						events: { signal: 'drag' },
+						update: '[drag[0] - start[0], start[1] - drag[1]]'
+					}
+				]
+			},
+			{
+				name: 'rotateX',
+				value: 0,
+				on: [
+					{
+						events: { signal: 'delta' },
+						update: 'angles[0] + delta[0]'
+					}
+				]
+			},
+			{
+				name: 'centerY',
+				value: 0,
+				on: [
+					{
+						events: { signal: 'delta' },
+						update: 'clamp(angles[1] + delta[1], -60, 60)'
+					}
+				]
 			}
 		],
 		data: [
@@ -53,7 +136,16 @@
 				]
 			}
 		],
-		projections: [{ name: 'projection', type: 'naturalEarth1' }],
+		projections: [
+			{
+				name: 'projection',
+				type: 'equalEarth',
+				scale: { signal: 'scale' },
+				rotate: [{ signal: 'rotateX' }, 0, 0],
+				center: [0, { signal: 'centerY' }],
+				translate: [{ signal: 'tx' }, { signal: 'ty' }]
+			}
+		],
 		scales: [
 			{
 				name: 'color',
