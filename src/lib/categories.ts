10f07dd97dd257f487f37780c46067e6b2962119
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-07-20 13:14:49 +0200

    Format

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,14 +1,17 @@
 export const categories = new Map([
-  ["C1",  "Limit warming to 1.5°C (>50%) with no or limited overshoot"],
-  ["C2",  "Return warming to 1.5°C (>50%) after a high overshoot"],
-  ["C3",  "Limit warming to 2°C (>67%)"],
-  ["C4",  "Limit warming to 2°C (>50%)"],
-  ["C5",  "Limit warming to 2.5°C (>50%)"],
-  ["C6",  "Limit warming to 3°C (>50%)"],
-  ["C7",  "Limit warming to 4°C (>50%)"],
-  ["C8",  "Exceed warming to 4°C (>50%)"],
-  ["C1+C2",  "Limit warming to 1.5°C (>50%) with no or limited overshoot + return warming to 1.5°C (>50%) after a high overshoot"],
-  ["C3+C4",  "Limit warming to 2°C (>67%) + limit warming to 2°C (>50%)"],
-  ["C5+C6",  "Limit warming to 2.5°C (>50%) + limit warming to 3°C (>50%)"],
-  ["C7+C8",  "Limit warming to 4°C (>50%) + exceed warming to 4°C (>50%)"],
-])
+	['C1', 'Limit warming to 1.5°C (>50%) with no or limited overshoot'],
+	['C2', 'Return warming to 1.5°C (>50%) after a high overshoot'],
+	['C3', 'Limit warming to 2°C (>67%)'],
+	['C4', 'Limit warming to 2°C (>50%)'],
+	['C5', 'Limit warming to 2.5°C (>50%)'],
+	['C6', 'Limit warming to 3°C (>50%)'],
+	['C7', 'Limit warming to 4°C (>50%)'],
+	['C8', 'Exceed warming to 4°C (>50%)'],
+	[
+		'C1+C2',
+		'Limit warming to 1.5°C (>50%) with no or limited overshoot + return warming to 1.5°C (>50%) after a high overshoot'
+	],
+	['C3+C4', 'Limit warming to 2°C (>67%) + limit warming to 2°C (>50%)'],
+	['C5+C6', 'Limit warming to 2.5°C (>50%) + limit warming to 3°C (>50%)'],
+	['C7+C8', 'Limit warming to 4°C (>50%) + exceed warming to 4°C (>50%)']
+]);
