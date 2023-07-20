c73371e22156b9af2f6e3233b8d45a95c1477aef
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-07-20 12:52:33 +0200

    Format

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,8 @@
 <h1 class="text-3xl font-bold">About explorer</h1>
 
 <p>
-    This is a prototype of a tool to explore the implications of different
-    principles for sharing the global carbon budget.
-
+	This is a prototype of a tool to explore the implications of different principles for sharing the
+	global carbon budget.
 </p>
 
 <p>...</p>
