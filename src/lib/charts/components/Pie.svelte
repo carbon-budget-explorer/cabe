e56bca105de34af849f5d02c279bdba8a618130e
sverhoeven <s.verhoeven@esciencecenter.nl>
2023-07-26 14:06:17 +0200

    Fix ts

--- src/lib/charts/components/Pie.svelte
+++ src/lib/charts/components/Pie.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { PieArcDatum } from 'd3';
 	import { scaleOrdinal } from 'd3-scale';
 	import { arc as d3arc } from 'd3-shape';
 	import { pie as d3pie } from 'd3-shape';
@@ -11,36 +12,36 @@
 
 	const radius = 250;
 
-	$: data = { used, remaining };
-	$: console.log(data);
+	type PieData = [string, number];
+	$: data = [
+		['used', used],
+		['remaining', remaining]
+	] as PieData[];
 
-	// const color = scaleOrdinal().range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
-	const color = scaleOrdinal().range(['red', 'green']);
+	$: color = scaleOrdinal<string>()
+		.range(['red', 'green'])
+		.domain(data.map((d) => d[0]));
 
 	// Compute the position of each group on the pie:
-	const pie = d3pie().value(function (d: any) {
-		return d[1];
-	});
-	$: data_ready = pie(Object.entries(data));
-    $: console.log(data_ready)
-	const arcGenerator = d3arc().innerRadius(0).outerRadius(radius);
+	const pie = d3pie<any, PieData>()
+		.sort(null)  // Don't sort
+		.sortValues(null) // Really don't sort
+		.value((d) => d[1]);
+	$: data_ready = pie(data);
+	const arcGenerator = d3arc<any, PieArcDatum<PieData>>().innerRadius(0).outerRadius(radius);
 </script>
 
-<div class="chart-container" id="my_dataviz">
+<div class="chart-container">
 	<svg {width} {height}>
 		<g transform="translate({width / 2}, {height / 2})">
-			{#each data_ready as slice, i}
-				<path
-					d={arcGenerator(slice)}
-                    fill={color(i)}
-					stroke="black"
-				/>
-                <text transform="translate({arcGenerator.centroid(slice)})">
-                    {slice.data[0]}
-                </text>
+			{#each data_ready as slice}
+				<path d={arcGenerator(slice)} fill={color(slice.data[0])} stroke="black" />
+				<text transform="translate({arcGenerator.centroid(slice)})">
+					{slice.data[0]}
+				</text>
 			{/each}
 		</g>
-    </svg>
+	</svg>
 </div>
 
 <style>
@@ -49,12 +50,12 @@
 		height: 500px;
 		margin: 40;
 	}
-    path {
-        stroke-width: style2px;
-        opacity: 0.7;
-    }
-    text {
-        text-anchor: middle;
-        font-size: 1.5em;
-    }
+	path {
+		stroke-width: style2px;
+		opacity: 0.7;
+	}
+	text {
+		text-anchor: middle;
+		font-size: 1.5em;
+	}
 </style>
