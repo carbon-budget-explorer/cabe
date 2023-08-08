2aff14c93aefd5717670581ff1f27e375bde3888
Peter Kalverla <peter.kalverla@gmx.com>
2023-08-08 16:19:53 +0200

    add new globals page

---
+++ src/routes/(nolayout)/global/+page.server.ts
@@ -0,0 +1,31 @@
+import { searchParam } from '$lib/searchparam';
+import type { PageServerLoad } from './$types';
+import {
+	warmingChoices,
+	type GlobalBudgetQuery,
+	nonCO2MitigationChoices as nonCO2MitigationChoices,
+	negativeEmissionsChoices,
+	probabilityChoices,
+	globalBudget
+} from '$lib/server/db/global';
+
+export const load = (async ({ url }: { url: URL }) => {
+	const query: GlobalBudgetQuery = {
+		warming: searchParam(url, 'warming', warmingChoices[0]),
+		probability: searchParam(url, 'probability', '50'),
+		nonCO2Mitigation: searchParam(url, 'nonCO2Mitigation', 'low'),
+		negativeEmissions: searchParam(url, 'negativeEmissions', 'low')
+	};
+	const choices = {
+		warming: warmingChoices,
+		nonCO2Mitigation: nonCO2MitigationChoices,
+		probability: probabilityChoices,
+		negativeEmissions: negativeEmissionsChoices
+	};
+	const result = globalBudget(query);
+	return {
+		query,
+		choices,
+		result
+	};
+}) satisfies PageServerLoad;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,30 +3,32 @@
 	import { page } from '$app/stores';
 </script>
 
-<nav class="flex flex-row space-x-8 bg-slate-100 p-4">
-	<a href={`/${$page.url.search}`}>Carbon Budget Explorer logo</a>
-	<ul class="flex flex-row space-x-6">
-		<li>
-			<a href={`/${$page.url.search}`} title="Home" class:font-bold={$page.url.pathname === '/'}
-				>Home</a
-			>
-		</li>
-		<li>
-			<a href={`/world${$page.url.search}`} class:font-bold={$page.url.pathname === '/world'}
-				>World</a
-			>
-		</li>
-		<li>
-			<a
-				href={`/world${$page.url.search}`}
-				class:font-bold={$page.url.pathname.startsWith('/region')}>Region</a
-			>
-		</li>
-		<li>
-			<a href="/about" class:font-bold={$page.url.pathname === '/about'}>About</a>
-		</li>
-	</ul>
-</nav>
-<div class="p-2">
-	<slot />
+<div class="h-[750px]">
+	<nav class="flex flex-row space-x-8 bg-slate-100 p-4">
+		<a href={`/${$page.url.search}`}>Carbon Budget Explorer logo</a>
+		<ul class="flex flex-row space-x-6">
+			<li>
+				<a href={`/${$page.url.search}`} title="Home" class:font-bold={$page.url.pathname === '/'}
+					>Home</a
+				>
+			</li>
+			<li>
+				<a href={`/world${$page.url.search}`} class:font-bold={$page.url.pathname === '/world'}
+					>World</a
+				>
+			</li>
+			<li>
+				<a
+					href={`/world${$page.url.search}`}
+					class:font-bold={$page.url.pathname.startsWith('/region')}>Region</a
+				>
+			</li>
+			<li>
+				<a href="/about" class:font-bold={$page.url.pathname === '/about'}>About</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="h-full p-2">
+		<slot />
+	</div>
 </div>
