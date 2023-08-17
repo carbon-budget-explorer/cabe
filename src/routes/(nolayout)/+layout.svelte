6e94cdf713cc3f9ca91365937df8d34130a9a5ae
sverhoeven <s.verhoeven@esciencecenter.nl>
2023-08-17 14:45:42 +0200

    Move stepper to layout

--- src/routes/(nolayout)/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="h-[750px]">
-	<slot />
-</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,66 @@
 <script lang="ts">
+	const activeli = 'flex items-center space-x-2.5 text-blue-600 dark:text-blue-500';
+	const passiveli = 'flex items-center space-x-2.5 text-gray-500 dark:text-gray-400';
+	const activespan =
+		'flex h-8 w-8 shrink-0 items-center justify-center rounded-full border border-blue-600 dark:border-blue-500';
+	const passivespan =
+		'flex h-8 w-8 shrink-0 items-center justify-center rounded-full border border-gray-500 dark:border-gray-400';
+
 	import '../app.css';
 	import { page } from '$app/stores';
 </script>
 
 <div class="h-[750px]">
 	<nav class="flex flex-row space-x-8 bg-slate-100 p-4">
-		<a href={`/${$page.url.search}`}>Carbon Budget Explorer logo</a>
-		<ul class="flex flex-row space-x-6">
-			<li>
-				<a href={`/${$page.url.search}`} title="Home" class:font-bold={$page.url.pathname === '/'}
-					>Home</a
-				>
-			</li>
-			<li>
-				<a href={`/world${$page.url.search}`} class:font-bold={$page.url.pathname === '/world'}
-					>World</a
-				>
-			</li>
-			<li>
-				<a
-					href={`/world${$page.url.search}`}
-					class:font-bold={$page.url.pathname.startsWith('/region')}>Region</a
-				>
-			</li>
-			<li>
-				<a href="/about" class:font-bold={$page.url.pathname === '/about'}>About</a>
-			</li>
-		</ul>
+		<div class="steps py-8">
+			<ol class="w-full items-center space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+				<li>
+					<a href={`/${$page.url.search}`}>Carbon Budget Explorer logo</a>
+				</li>
+				<!-- TODO add arrow between stepper items -->
+				<li class={$page.url.pathname === '/global' ? activeli : passiveli}>
+					<span class={$page.url.pathname === '/global' ? activespan : passivespan}> 1 </span>
+					<span>
+						<h3 class="font-medium leading-tight">
+								<a href="/global">
+								Construct your scenario
+							</a>
+							</h3>
+						</span>
+				</li>
+				<li class={$page.url.pathname === '/world' ? activeli : passiveli}>
+					<span class={$page.url.pathname === '/world' ? activespan : passivespan}> 2 </span>
+					<span>
+						<h3 class="font-medium leading-tight">
+							<a href="/world">
+								Choose your effort-sharing method
+							</a>
+							</h3>
+						</span>
+					</li>
+				<li class={$page.url.pathname.startsWith('/region') ? activeli : passiveli}>
+					<span class={$page.url.pathname.startsWith('/region') ? activespan : passivespan}>
+						3
+					</span>
+					<span>
+							<a href="/region">
+							<h3 class="font-medium leading-tight">Get country details</h3>
+						</a>
+						</span>
+				</li>
+				<li class={$page.url.pathname.startsWith('/compare') ? activeli : passiveli}>
+					<span class={$page.url.pathname.startsWith('/compare') ? activespan : passivespan}>
+						4
+					</span>
+					<span>
+						<h3 class="font-medium leading-tight">Compare</h3>
+					</span>
+				</li>
+				<li class="">
+					<a href="/about" title="About" class:font-bold={$page.url.pathname === '/about'}>?</a>
+				</li>
+			</ol>
+		</div>
 	</nav>
 	<div class="h-full p-2">
 		<slot />
