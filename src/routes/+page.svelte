4962868f08f0db3d9c74fb955365d59e5fdd7bce
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-08-18 13:47:47 +0200

    Style button on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1 class="pb-20 text-3xl font-bold">Welcome to Carbon Budget Explorer</h1>
 
 <div class="flex flex-row justify-around">
-	<div class="flex flex-col justify-between gap-2">
-		<a href="/global">Construct your pathway</a>
+	<div class="flex flex-col justify-between gap-2 ">
+		<a class="mb-2 mr-2 block rounded-lg bg-slate-300 px-5 py-2.5 text-center text-3xl font-medium text-white hover:bg-slate-400 focus:outline-none focus:ring-4" href="/global">Construct your pathway</a>
 	</div>
 </div>
