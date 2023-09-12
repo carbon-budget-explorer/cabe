d4087eee8a286a8e1b36b13e377bfa6da692c563
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-09-12 08:34:29 +0200

    Update svelte-leafletjs?client.d.ts

--- src/svelte-leafletjs?client.d.ts
+++ src/svelte-leafletjs?client.d.ts
@@ -1,3 +1,5 @@
+// Add VS Code integration for ?client
+// see https://github.com/bluwy/vite-plugin-iso-import
 declare module 'svelte-leafletjs?client' {
 	import * as all from 'svelte-leafletjs';
 	export = all;
