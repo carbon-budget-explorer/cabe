fd6acd7e8c9af8e3714798ebedda2427efb17faf
Peter Kalverla <peter.kalverla@gmx.com>
2023-09-21 10:39:03 +0200

    sliders... not working great yet, but it's a start

---
+++ src/lib/CustomRange.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let value: string;
+	export let options: string[];
+	export let name: string;
+
+	type ChangeEvent = Event & {
+		currentTarget: EventTarget & HTMLInputElement;
+	};
+
+	function updateValue(event: ChangeEvent) {
+		value = options[event.currentTarget.valueAsNumber];
+	}
+
+	$: len = options.length;
+	$: valIndex = options.indexOf(value);
+	$: console.log(value, valIndex, options[valIndex]);
+</script>
+
+<label>
+	<input
+		type="range"
+		{name}
+		min="0"
+		max={len - 1}
+		step="1"
+		value={valIndex}
+		on:change={updateValue}
+	/>
+	{value}
+</label>
