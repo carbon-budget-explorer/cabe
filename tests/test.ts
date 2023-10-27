b51d42b3d6f1b45eb5233fb8e5bca5446b2d5b15
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-10-27 16:04:54 +0200

    Format

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected link', async ({ page }) => {
 	await page.goto('/');
-	await expect(
-		page.getByRole('link', { name: /Construct your pathway/ })
-	).toBeVisible();
+	await expect(page.getByRole('link', { name: /Construct your pathway/ })).toBeVisible();
 });
