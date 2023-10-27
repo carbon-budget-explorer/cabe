89eaf944f69f56531c2eb93ab31624c8d3a67836
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-10-27 12:47:30 +0200

    Prettify landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
+	import logo from '$lib/logo.svg';
 </script>
 
+
 <div class="flex h-screen max-h-screen flex-col" data-theme="corporate">
+<!-- TODO or have navbar for root page -->
+{#if $page.url.pathname ==='/'}
+  <slot />
+{:else}
 	<div class="navbar bg-primary">
 		<div class="flex-1">
 			<a href={`/${$page.url.search}`} class="btn-ghost btn text-xl normal-case"
-				>Logo | Carbon Budget Explorer</a
+				><img src={logo} class="h-full" alt="Logo for Carbon Budget Explorer" /> | Carbon Budget Explorer</a
 			>
 		</div>
 		<div class="flex-none">
-			<button class="btn-ghost btn-square btn px-10">About</button>
+			<a href="/about" class="btn-ghost btn-square btn px-10">About</a>
 		</div>
 	</div>
 
 	<div class="grow bg-base-200 p-4">
 		<slot />
 	</div>
+	{/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
-<h1 class="pb-20 text-3xl font-bold">Welcome to Carbon Budget Explorer</h1>
+<script lang="ts">
+	import logo from '$lib/logo.svg';
+	export const prerender = true;
+</script>
 
-<div class="flex flex-row justify-around">
-	<div class="flex flex-col justify-between gap-2">
-		<a
-			class="mb-2 mr-2 block rounded-lg bg-slate-300 px-5 py-2.5 text-center text-3xl font-medium text-white hover:bg-slate-400 focus:outline-none focus:ring-4"
-			href="/global">Construct your pathway</a
-		>
+<main class="grid h-full grid-cols-2 content-around gap-20 p-40 bg-base-100 ">
+	<div class="flex flex-row">
+		<img src={logo} class="h-full" alt="Logo for Carbon Budget Explorer" />
+		<div class="text-6xl font-bold">
+			<div>Carbon</div>
+			<div>Budget</div>
+			<div>Explorer</div>
+		</div>
 	</div>
-</div>
+	<a
+		class="btn btn-primary btn-lg rounded-lg self-center"
+		href="/global"
+	>
+		Construct your pathway &raquo;
+	</a>
+
+	<div class="col-span-2 text-2xl">
+		Welcome to the Carbon Budget Explorer. This tool allows you to explore the carbon budget for the
+		world, and find how each country can contribute to the global effort to limit warming.
+	</div>
+</main>
