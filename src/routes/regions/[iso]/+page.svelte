2b7fa57d16d919619aee26e55c00f27d81ffaab9
Peter Kalverla <peter.kalverla@gmx.com>
2023-10-27 08:05:54 +0200

    Country header tweaking

--- src/routes/regions/[iso]/+page.svelte
+++ src/routes/regions/[iso]/+page.svelte
@@ -81,8 +81,15 @@
 		<MiniPathwayCard global={data.global} />
 	</div>
 	<div class="flex h-full grow flex-col">
-		<div id="country-header" class="flex flex-row items-center gap-4">
-			<a href={`/map${$page.url.search}`} title="Back to map" class="text-8xl"> ← </a>
+		<div id="country-header" class="flex flex-row items-center gap-4 pb-2">
+			<a href={`/map${$page.url.search}`} title="Back to map"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"
+					><path
+						fill="currentColor"
+						d="M128 20a108 108 0 1 0 108 108A108.12 108.12 0 0 0 128 20Zm0 192a84 84 0 1 1 84-84a84.09 84.09 0 0 1-84 84Zm52-84a12 12 0 0 1-12 12h-51l11.52 11.51a12 12 0 0 1-17 17l-32-32a12 12 0 0 1 0-17l32-32a12 12 0 0 1 17 17L117 116h51a12 12 0 0 1 12 12Z"
+					/></svg
+				>
+			</a>
 			<img
 				src={`https://flagcdn.com/${data.info.iso2?.toLowerCase()}.svg`}
 				class="h-8"
