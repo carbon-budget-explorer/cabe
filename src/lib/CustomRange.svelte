8c8f8a42a400a45d41aa4122b64c357d0cd4fbc3
Peter Kalverla <peter.kalverla@gmx.com>
2023-10-31 17:49:49 +0100

    start spacing the sliders

--- src/lib/CustomRange.svelte
+++ src/lib/CustomRange.svelte
@@ -18,7 +18,7 @@
 </script>
 
 <label>
-	<div class="flox-row flex gap-2">
+	<div class="flex flex-row gap-2">
 		<input
 			type="range"
 			class="range range-accent range-xs"
@@ -29,8 +29,14 @@
 			value={valIndex}
 			on:change={updateValue}
 		/>
-		<div class="badge- badge">
-			{value}
-		</div>
+	</div>
+	<div class="flex w-full justify-between px-2 text-xs">
+		<!-- TODO make low/high settable to numbers/strings -->
+		<span>low</span>
+		<span>high</span>
+	</div>
+	<!-- TODO make value show up at place of slider? -->
+	<div class="badge- badge">
+		{value}
 	</div>
 </label>
