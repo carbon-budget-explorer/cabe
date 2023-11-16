54572eec74b04ccaa23df72bba8bf23bde5f1687
Peter Kalverla <peter.kalverla@gmx.com>
2023-11-16 13:33:14 +0100

    start restructuring about

---
+++ src/lib/QA.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	let counter = 0;
+</script>
+
+<script lang="ts">
+	counter += 1;
+	let id: string = 'question-' + counter;
+	export let question: string;
+</script>
+
+<div>
+	<input {id} type="checkbox" class="group peer hidden" />
+	<label for={id} class="cursor-pointer"
+		><h3 class="rounded p-2">
+			<span>⮚</span>
+			{question}
+		</h3></label
+	>
+	<div class="hidden px-2 peer-checked:block">
+		<slot><p>Answer</p></slot>
+	</div>
+</div>
+
+<!-- content-['︾'] content-['»'] -->
+<!-- ⮛ -->
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,37 +1,62 @@
 <script lang="ts">
+	import Answer from '$lib/Answer.svelte';
+	import Question from '$lib/Question.svelte';
+	import QA from '$lib/QA.svelte';
+
 	export const prerender = true;
 </script>
 
-<div class="flex gap-4">
-	<div class="prose bg-base-100 p-4 shadow-xl">
-		<h2>Science</h2>
-		<h3>Global carbon budget</h3>
-		<p>
-			The global carbon budget numbers are taken from Forster et al. (2023,
-			https://essd.copernicus.org/articles/15/2295/2023/), who summarize updates of the WGI and
-			WGIII reports of the IPCC, and updates of climate emulators. This paper directly reports CO2
-			budgets, but also the implied paths on non-CO2. The latter is used, augmented with information
-			on cost-optimal global pathways from the AR6 database, to infer yearly total greenhouse gas
-			emissions over the coming century.
-		</p>
-		<h3>Effort-sharing methods</h3>
-		<p>
-			Links to Van der Berg et al. and Robiou. Picture of allocations.
-			<img src="/paper_preview.png" alt="paper preview" />
-		</p>
+<!-- Sidebar -->
+<div class="box-border flex h-full gap-4 bg-base-300">
+	<div class="items-between flex h-full w-1/5 flex-col bg-base-300">
+		<div class="card card-compact">
+			<div class="card-body prose">
+				<h2 class="card-title">Index</h2>
+				<ul>
+					<li>Scientific basis</li>
+					<li>Societal relevance</li>
+					<li>Technological aspects</li>
+				</ul>
+			</div>
+		</div>
+		<div class="card-compact card mt-auto">
+			<div class="card-body prose">
+				<h2 class="card-title">Contact</h2>
+				<p>
+					For more information you can contact the lead author of the scientific research on fair
+					shares dr. Mark Dekker at mark.dekker@pbl.nl
+				</p>
+			</div>
+		</div>
 	</div>
+	<!-- Main panel -->
+	<main class="max-h-full flex-1 bg-base-100 p-4">
+		<div class="prose ml-4 max-w-[60%]">
+			<h1>Frequently asked questions</h1>
+			<h2>Scientific basis</h2>
 
-	<div class="prose bg-base-100 p-4 shadow-xl">
-		<h2>More information</h2>
-		<p>Paper / pre-print link</p>
-		<p>Something on the code / Github</p>
-	</div>
+			<!-- <Question></Question>
+			<Answer>
 
-	<div class="prose bg-base-100 p-4 shadow-xl">
-		<h2>Contact</h2>
-		<p>
-			For more information you can contact the lead author of the scientific research on fair shares
-			dr. Mark Dekker at mark.dekker@pbl.nl
-		</p>
-	</div>
+			</Answer>
+
+			<Question></Question>
+			<Answer>
+
+			</Answer> -->
+			<QA question="Where do the numbers on the global carbon budget come from?">
+				The global carbon budget numbers are taken from Forster et al. (2023,
+				https://essd.copernicus.org/articles/15/2295/2023/), who summarize updates of the WGI and
+				WGIII reports of the IPCC, and updates of climate emulators. This paper directly reports CO2
+				budgets, but also the implied paths on non-CO2. The latter is used, augmented with
+				information on cost-optimal global pathways from the AR6 database, to infer yearly total
+				greenhouse gas emissions over the coming century.
+			</QA>
+			<QA question="What are these effort-sharing principles?">
+				Links to Van der Berg et al. and Robiou. Picture of allocations.
+				<img src="/paper_preview.png" alt="paper preview" />
+			</QA>
+			<QA question="this is the question" />
+		</div>
+	</main>
 </div>
