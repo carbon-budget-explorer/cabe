e81f4404dfda7fbf911c1eac63a8652ca6c37d2d
Laszlo Vreedenburgh <laszlo.vreedenburgh@gmail.com>
2023-11-29 12:47:26 +0100

    formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,10 @@
 					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 				/></svg
 			>
-			<span>This is a beta version of the Explorer and is subject to frequent updates. Please see our About page.</span>
+			<span
+				>This is a beta version of the Explorer and is subject to frequent updates. Please see our
+				About page.</span
+			>
 		</div>
 		<div class="flex-none">
 			<a href="/about" class="btn-ghost btn-square btn px-10">About</a>
