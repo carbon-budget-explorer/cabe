c5912d67835f5639f56c5dc8164616d16151b052
Laszlo Vreedenburgh <laszlo.vreedenburgh@gmail.com>
2023-11-29 12:36:25 +0100

    Move beta banner to top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,21 @@
 				/> Carbon Budget Explorer</a
 			>
 		</div>
+		<div class="alert" style="width: 55%">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="h-6 w-6 shrink-0 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>This is a beta version of the Explorer and is subject to frequent updates. Please see our About page.</span>
+		</div>
 		<div class="flex-none">
 			<a href="/about" class="btn-ghost btn-square btn px-10">About</a>
 		</div>
