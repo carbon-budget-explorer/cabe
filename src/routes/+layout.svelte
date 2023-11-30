d97a1f3adc6a5baaf0550675dc877807ad205618
Laszlo Vreedenburgh <laszlo.vreedenburgh@gmail.com>
2023-11-30 15:21:42 +0100

    smaller banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 				/> Carbon Budget Explorer</a
 			>
 		</div>
-		<div class="alert" style="width: 55%">
+		<div class="alert" style="width: 50%">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
