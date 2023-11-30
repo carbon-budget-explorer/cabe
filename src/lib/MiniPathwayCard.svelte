9ef0c42d52835fbcccaed7393587daaaa9047065
Laszlo Vreedenburgh <laszlo.vreedenburgh@gmail.com>
2023-11-30 14:44:24 +0100

    Y axes

--- src/lib/MiniPathwayCard.svelte
+++ src/lib/MiniPathwayCard.svelte
@@ -30,7 +30,7 @@
 			<!-- TODO on x-axis have less or no ticks, now they are overlapping and unreadable -->
 			<!-- TODO would be cool when you navigate from /global to /map this chart would view transition from full screen to minimap -->
 			<!-- TODO would be nice when you hover over line or area it would show tooltip with series description -->
-			<Pathway xTicks={3} yTicks={4}>
+			<Pathway xTicks={3} yTicks={4} yAxisTtle="Greenhouse gas emissions (Gt CO₂e/year)">
 				<Line data={global.historicalCarbon} x={'time'} y={'value'} color="black" />
 				<Line data={$pathwayCarbonTweened} x={'time'} y={'mean'} color={ipcc_green} />
 				<Area data={$pathwayCarbonTweened} x={'time'} y0={'min'} y1={'max'} color={ipcc_green} />
